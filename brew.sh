63be6a2d39eaa8ef385b1f8faa11216ed804101a
Gerlito Chagas <gerlito.chagas@gmail.com>
2025-10-19 10:29:47 +0800

    feat: add applications to brew.sh

--- brew.sh
+++ brew.sh
@@ -1,4 +1,20 @@
 # add on top of .zshrc if on brew
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
+brew install curl
+brew install fzf
+brew install hadolint
+brew install mise
+brew install tree
 brew install tlrc # tldr docs
+
+
+brew install --cask alt-tab
+brew install --cask betterdisplay
+brew install --cask bitwarden
+brew install --cask ghostty
+brew install --cask linearmouse
+brew install --cask middleclick
+brew install --cask obs
+brew install --cask rectangle
+brew install --cask stats
